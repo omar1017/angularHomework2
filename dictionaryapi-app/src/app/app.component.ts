d8822acae4859fa52086032be4d8dfa88de52eb2
AbdAlkaderKwayyes <abdalkaderkwayyes192@gmail.com>
2024-09-09 15:41:21 +0300

    This is the initial design where a folder has been created for the dictionary component.

--- dictionaryapi-app/src/app/app.component.ts
+++ dictionaryapi-app/src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TabsModule } from 'ngx-bootstrap/tabs';
+import { DictionaryComponent } from "./dictionary/dictionary.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
+  imports: [RouterOutlet, TabsModule, DictionaryComponent],
+  template: `<app-dictionary></app-dictionary>`,
   styleUrl: './app.component.css'
 })
 export class AppComponent {
